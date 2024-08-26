145f8b022d78bdad3be9a828c77c8f460e4e3401
Vaibhav Kubre <v@kubre.in>
2024-08-26 19:46:30 +0530

    Can check if something is selected

--- game/main.c
+++ game/main.c
@@ -1,12 +1,12 @@
 typedef enum EntityType {
-    Entity_NIL,
-    Entity_SKILLET,
-    Entity_TEA,
-    Entity_TEA_POWDER,
-    Entity_WATER,
-    Entity_MILK,
-    Entity_SUGAR,
-    Entity_STOVE,
+    Entity_NIL        = 0,
+    Entity_SKILLET    = 1,
+    Entity_TEA        = 2,
+    Entity_TEA_POWDER = 3,
+    Entity_WATER      = 4,
+    Entity_MILK       = 5,
+    Entity_SUGAR      = 6,
+    Entity_STOVE      = 7,
 } EntityType;
 
 
@@ -14,7 +14,7 @@ typedef struct Entity {
     Gfx_Image* image;
     EntityType type;
     bool       is_valid;
-    Vector2    pos;
+    Vector3    pos;
 } Entity;
 
 
@@ -51,20 +51,40 @@ int entry(int argc, char **argv) {
 	window.y = 90;
 	window.clear_color = hex_to_rgba(0x222034ff);
 
-    Gfx_Image *skillet    = load_image_from_disk(
-                                STR("sprites/skillet.png"), get_heap_allocator());
-    Gfx_Image *milk       = load_image_from_disk(
-                                STR("sprites/milk.png"), get_heap_allocator());
-    Gfx_Image *stove      = load_image_from_disk(
-                                STR("sprites/stove.png"), get_heap_allocator());
-    Gfx_Image *sugar      = load_image_from_disk(
-                                STR("sprites/sugar.png"), get_heap_allocator());
-    Gfx_Image *tea_powder = load_image_from_disk(
-                                STR("sprites/tea_powder.png"), get_heap_allocator());
-    Gfx_Image *tea        = load_image_from_disk(
-                                STR("sprites/tea.png"), get_heap_allocator());
-    Gfx_Image *water      = load_image_from_disk(
-                                STR("sprites/water.png"), get_heap_allocator());
+    world = alloc(get_heap_allocator(), sizeof(World));
+
+    Entity* skillet = Entity_create();
+    skillet->image  = load_image_from_disk(STR("sprites/skillet.png"), get_heap_allocator());
+    skillet->pos    = v3(-80, -40, 0);
+    skillet->type   = Entity_SKILLET;
+
+    Entity* milk = Entity_create();
+    milk->image  = load_image_from_disk(STR("sprites/milk.png"), get_heap_allocator());
+    milk->pos    = v3(-120, 0, 0);
+    milk->type   = Entity_MILK;
+
+    Entity* stove = Entity_create();
+    stove->image  = load_image_from_disk(STR("sprites/stove.png"), get_heap_allocator());
+    stove->pos    = v3(0, -20, 0);
+    stove->type   = Entity_STOVE;
+
+    Entity* sugar = Entity_create();
+    sugar->image  = load_image_from_disk(STR("sprites/sugar.png"), get_heap_allocator());
+    sugar->pos    = v3(-120, -20, 0);
+    sugar->type   = Entity_SUGAR;
+
+    Entity* tea_powder = Entity_create();
+    tea_powder->image  = load_image_from_disk(STR("sprites/tea_powder.png"), get_heap_allocator());
+    tea_powder->pos    = v3(-120, -40, 0)    ;
+    tea_powder->type   = Entity_TEA_POWDER;
+
+    Entity* tea = Entity_create();
+    tea->image  = load_image_from_disk(STR("sprites/tea.png"), get_heap_allocator());
+    tea->pos    = v3(20, 50, 0);
+    tea->type   = Entity_TEA;
+
+    // Entity* water = Entity_create();
+    // water->image  = load_image_from_disk(STR("sprites/water.png"), get_heap_allocator());
 
     assert(skillet, "skillet.png cannot be loaded!");
     assert(milk, "milk.png cannot be loaded!");
@@ -72,14 +92,13 @@ int entry(int argc, char **argv) {
     assert(sugar, "sugar.png cannot be loaded!");
     assert(tea_powder, "tea_powder.png cannot be loaded!");
     assert(tea, "tea.png cannot be loaded!");
-    assert(water, "water.png cannot be loaded!");
+    // assert(water, "water.png cannot be loaded!");
 
     f32 zoom = 4.f;
 
     Vector2 last_pos = v2(0, 0);
 	while (!window.should_close) {
 		reset_temporary_storage();
-
         draw_frame.projection = m4_make_orthographic_projection(
                                     window.width * -.5f, window.width * .5f,
                                     window.height * -.5f, window.height * .5f,
@@ -91,59 +110,71 @@ int entry(int argc, char **argv) {
             window.should_close = true;
         }
 
+        {
+            Matrix4 xform = m4_scalar(1.0);
+            xform = m4_translate(xform, skillet->pos);
+            draw_image_xform(skillet->image, xform,
+                             v2(skillet->image->width, skillet->image->height), COLOR_WHITE);
+        }
+
+        {
+            Matrix4 xform = m4_scalar(1.0);
+            xform = m4_translate(xform, tea_powder->pos);
+            draw_image_xform(tea_powder->image, xform, v2(tea_powder->image->width, tea_powder->image->height), COLOR_WHITE);
+        }
+
+        {
+            Matrix4 xform = m4_scalar(1.0);
+            xform = m4_translate(xform, sugar->pos);
+            draw_image_xform(sugar->image, xform, v2(sugar->image->width, sugar->image->height), COLOR_WHITE);
+        }
+
+        {
+            Matrix4 xform = m4_scalar(1.0);
+            xform = m4_translate(xform, milk->pos);
+            draw_image_xform(milk->image, xform, v2(milk->image->width, milk->image->height), COLOR_WHITE);
+        }
+
+        {
+            Matrix4 xform = m4_scalar(1.0);
+            xform = m4_translate(xform, tea->pos);
+            draw_image_xform(tea->image, xform, v2(tea->image->width, tea->image->height), COLOR_WHITE);
+        }
+
+        {
+            Matrix4 xform = m4_scalar(1.0);
+            xform = m4_translate(xform, stove->pos);
+            draw_image_xform(stove->image, xform, v2(stove->image->width, stove->image->height), COLOR_WHITE);
+        }
 
         Vector2 mouse_pos = v2(
             (input_frame.mouse_x - (f32)window.width * .5f) / zoom,
             (input_frame.mouse_y - (f32)window.height * .5f) / zoom);
-        Vector3 skillet_pos = v3((last_pos.x - (f32)skillet->width * .5f),
-                                 (last_pos.y - (f32)skillet->height * .5f), 0);
-
-        {
-            Matrix4 xform = m4_scalar(1.0);
-            xform = m4_translate(xform, v3(-80, -40, 0));
-            draw_image_xform(skillet, xform,
-                             v2(skillet->width, skillet->height), COLOR_WHITE);
+        Entity* selected_entity = 0;
+        if (is_key_down(MOUSE_BUTTON_LEFT)) {
+            for (int i = 0; i < MAX_ENTITIES_COUNT; ++i) {
+                Entity* entity_i = &world->entities[i];
+                if ((mouse_pos.x > entity_i->pos.x) &&
+                    (mouse_pos.x < entity_i->pos.x + (f32)entity_i->image->width) &&
+                    (mouse_pos.y > entity_i->pos.y) &&
+                    (mouse_pos.y < entity_i->pos.y + (f32)entity_i->image->height)) {
+                        selected_entity = entity_i;
+                        printf("Selcted entity %d", selected_entity->type);
+                        break;
+                }
+            }
         }
-
-        {
-            Matrix4 xform = m4_scalar(1.0);
-            xform = m4_translate(xform, v3(-120, -40, 0));
-            draw_image_xform(tea_powder, xform, v2(tea_powder->width, tea_powder->height), COLOR_WHITE);
-        }
-
-        {
-            Matrix4 xform = m4_scalar(1.0);
-            xform = m4_translate(xform, v3(-120, -20, 0));
-            draw_image_xform(sugar, xform, v2(sugar->width, sugar->height), COLOR_WHITE);
-        }
-
-        {
-            Matrix4 xform = m4_scalar(1.0);
-            xform = m4_translate(xform, v3(-120, 0, 0));
-            draw_image_xform(milk, xform, v2(milk->width, milk->height), COLOR_WHITE);
-        }
-
-        {
-            Matrix4 xform = m4_scalar(1.0);
-            xform = m4_translate(xform, v3(20, 50, 0));
-            draw_image_xform(tea, xform, v2(tea->width, tea->height), COLOR_WHITE);
-        }
-
-        {
-            Matrix4 xform = m4_scalar(1.0);
-            xform = m4_translate(xform, v3(0, -20, 0));
-            draw_image_xform(stove, xform, v2(stove->width, stove->height), COLOR_WHITE);
-        }
-
-        if (is_key_down(MOUSE_BUTTON_LEFT) &&
-            (mouse_pos.x > skillet_pos.x) &&
-            (mouse_pos.x < skillet_pos.x + (f32)skillet->width) &&
-            (mouse_pos.y > skillet_pos.y) &&
-            (mouse_pos.y < skillet_pos.y + (f32)skillet->height)) {
-            skillet_pos = v3(mouse_pos.x - (f32)skillet->width * .5f,
-                             mouse_pos.y - (f32)skillet->height * .5f, 0);
-            last_pos = v2(mouse_pos.x, mouse_pos.y);
-        }
+        // Vector3 skillet_pos = v3((last_pos.x - (f32)skillet->width * .5f),
+        //                          (last_pos.y - (f32)skillet->height * .5f), 0);
+        // if (is_key_down(MOUSE_BUTTON_LEFT) &&
+        //     (mouse_pos.x > skillet_pos.x) &&
+        //     (mouse_pos.x < skillet_pos.x + (f32)skillet->width) &&
+        //     (mouse_pos.y > skillet_pos.y) &&
+        //     (mouse_pos.y < skillet_pos.y + (f32)skillet->height)) {
+        //     skillet_pos = v3(mouse_pos.x - (f32)skillet->width * .5f,
+        //                      mouse_pos.y - (f32)skillet->height * .5f, 0);
+        //     last_pos = v2(mouse_pos.x, mouse_pos.y);
+        // }
 		gfx_update();
 	}
 
